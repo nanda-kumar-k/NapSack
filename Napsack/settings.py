b9907f468ab32e778ddd3e11af192028bfd71f3e
N-and-U <nandakumar.k05@gmail.com>
2022-05-30 14:01:18 +0530

    setting update

--- Napsack/settings.py
+++ Napsack/settings.py
@@ -13,7 +13,9 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -146,8 +148,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+# STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATIC_ROOT = 'D:/home/site/wwwroot/static'
 
@@ -167,6 +176,9 @@ CSRF_TRUSTED_ORIGINS = ['https://napsack.azurewebsites.net']
 
 MEDIA_URL = '/media/'
 # Path where media is stored
+
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
